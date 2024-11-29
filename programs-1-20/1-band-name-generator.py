b02b1ff616cda877938f84d9853cade202d6b4f5
pranjal <pranjalsarnaikan@gmail.com>
2024-11-29 18:21:08 +0530

    Add programs-1-20 folder which contains my first two practice python programs

---
+++ programs-1-20/1-band-name-generator.py
@@ -0,0 +1,18 @@
+print('-'*40)
+print('Welcome to band name generator')
+print('-'*40)
+print(" ")
+
+city_name = input('Please enter the name of city you grew up:\n')
+pet_name = input('Please enter the name of your pet:\n')
+
+print(" ")
+print('-'*40)
+
+print(" ")
+print("The name of your band could be " + city_name + pet_name)
+print(" ")
+print('-'*40)
+
+print(" ")
+print('End of program, thank you for using our service!')
